d3b668641f06c1d93443bcd59d02b5f77a607db3
jazzrider <root@nat-tvwna-inside-visitornet101-c-23842.princeton.org>
2016-04-02 07:16:34 -0400

    added fgen double,half,triple

--- testbench/skills/__init__.py
+++ testbench/skills/__init__.py
@@ -5,6 +5,7 @@ from skills.nullskill import NullSkill
 from skills.resistor import Resistor, RevResistor
 from skills.octospec import OctoSpec
 from skills.oscope import OSCOPEAutoset, OSCOPEImage, OSCOPESetVdiv, OSCOPEMeasure
+from skills.fgen import FGENDouble, FGENHalf, FGENTriple
 
 skillmap = {}
 skillmap["AMAZON.HelpIntent"] = Help()
@@ -17,3 +18,6 @@ skillmap["OSCOPEImage"] = OSCOPEImage()
 skillmap["OSCOPEAutoset"] = OSCOPEAutoset()
 skillmap["OSCOPESetVdiv"] = OSCOPESetVdiv()
 skillmap["OSCOPEMeasure"] = OSCOPEMeasure()
+skillmap["FGENDouble"] = FGENDouble()
+skillmap["FGENHalf"] = FGENHalf()
+skillmap["FGENTriple"] = FGENTriple()
--- testbench/skills/fgen.py
+++ testbench/skills/fgen.py
@@ -1,1 +1,60 @@
+import util
+from skills.skill import SkillBase
+import redis
+import json
+import random
+import string
+
+types = {"frequency":'frequency',"voltage":'voltage',"voltage offset":'voltageoffset'}
+
+class FGENDouble(SkillBase):
+
+    def execute(__self__, intent, session):
+        t = types[intent['slots']['type']['value']]
+        command = ['double', t]
+        r = redis.Redis("104.236.205.31")
+        r.publish("boss",json.dumps(command))
+        session_attributes = {}
+        card_title = None
+        speech_output = 'doubling the ' + t
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
+
+class FGENHalf(SkillBase):
+
+    def execute(__self__, intent, session):
+        t = types[intent['slots']['type']['value']]
+        command = ['half', t]
+        r = redis.Redis("104.236.205.31")
+        r.publish("boss",json.dumps(command))
+        session_attributes = {}
+        card_title = None
+        speech_output = 'halving the ' + t
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
+
+class FGENTriple(SkillBase):
+
+    def execute(__self__, intent, session):
+        t = types[intent['slots']['type']['value']]
+        command = ['triple', t]
+        r = redis.Redis("104.236.205.31")
+        r.publish("boss",json.dumps(command))
+        session_attributes = {}
+        card_title = None
+        speech_output = 'tripling the ' + t
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
 
