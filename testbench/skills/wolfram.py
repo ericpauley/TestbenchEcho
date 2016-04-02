a284c22c46146936d8dfda7347c703fa80f4e1b1
venividivici440 <venividivici440@gmail.com>
2016-04-02 10:42:02 -0400

    asdfsadf

--- testbench/skills/wolfram.py
+++ testbench/skills/wolfram.py
@@ -23,8 +23,8 @@ class Wolfram(SkillBase):
         speech = ""
         for pod in root.findall('.//pod'):
             speech += pod.attrib['title']
-            if pod.attrib['title'] == 'Result' or pod.attrib['title'] == 'Circuit diagram'
-                for im in pod.findall('.//img')
+            if pod.attrib['title'] == 'Result' or pod.attrib['title'] == 'Circuit diagram':
+                for im in pod.findall('.//img'):
                     image = im.attrib['src']
             for pt in pod.findall('.//plaintext'):
                 if pt.text:
