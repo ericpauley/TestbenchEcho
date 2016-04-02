b37a2e8c19b9a42f6e7d487af5d1cb4f2d09a652
jazzrider <root@nat-tvwna-inside-visitornet101-c-23842.princeton.org>
2016-04-02 07:58:06 -0400

    more freq skills

--- testbench/skills/fgen.py
+++ testbench/skills/fgen.py
@@ -7,6 +7,7 @@ import string
 
 types = {"frequency":'frequency',"voltage":'voltage',"voltage offset":'voltageoffset'}
 funits = {"Hertz":1,"kilohertz":1000,"megahertz":100000}
+vunits = {"volts":1,"volt":1,"millivolts":.001,"millivolt":.001}
 
 class FGENDouble(SkillBase):
 
@@ -69,6 +70,40 @@ class FGENSetFrequency(SkillBase):
         session_attributes = {}
         card_title = None
         speech_output = 'changing the frequency to ' + intent['slots']['value']['value'] + intent['slots']['funits']['value']
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
+
+class FGENSetVoltage(SkillBase):
+
+    def execute(__self__, intent, session):
+        value = int(intent['slots']['value']['value']) * vunits[intent['slots']['vunits']['value']]
+        command = ['voltage', str(value)]
+        r = redis.Redis("104.236.205.31")
+        r.publish("boss",json.dumps(command))
+        session_attributes = {}
+        card_title = None
+        speech_output = 'changing the voltage to ' + intent['slots']['value']['value'] + intent['slots']['funits']['value']
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
+
+class FGENSetVoltageOffset(SkillBase):
+
+    def execute(__self__, intent, session):
+        value = int(intent['slots']['value']['value']) * vunits[intent['slots']['vunits']['value']]
+        command = ['voltageoffset', str(value)]
+        r = redis.Redis("104.236.205.31")
+        r.publish("boss",json.dumps(command))
+        session_attributes = {}
+        card_title = None
+        speech_output = 'changing the voltage offset to ' + intent['slots']['value']['value'] + intent['slots']['funits']['value']
         # If the user either does not reply to the welcome message or says something
         # that is not understood, they will be prompted again with this text.
         reprompt_text = None
