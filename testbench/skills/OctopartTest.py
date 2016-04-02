108e09a0af14ac23cb8a09b070d628e54461120d
venividivici440 <venividivici440@gmail.com>
2016-04-02 02:40:24 -0400

    Super Jank Octopart Test

---
+++ testbench/skills/OctopartTest.py
@@ -0,0 +1,49 @@
+import json
+import urllib
+import util
+from skills.skill import SkillBase
+
+
+
+specMap = {"Input Voltage (DC)": 'input_voltage_dc'}
+
+class OctopartTest(SkillBase)
+    def execute(__self__, intent, session):
+        session_attributes = {}
+        val = ""
+        url = 'http://octopart.com/api/v3/parts/match?'
+        url += '&apikey=0c491965'
+        url += '&pretty_print=false'
+        url += '&queries=[{"mpn":"LF412CD"}]'
+        url += '&include[]=specs'
+
+
+        data = urllib.urlopen(url).read()
+        response = json.loads(data)
+
+        val = response[items][0]['specs'][specMap[intent[slots][specs]]]['display_value']
+
+        speech_output = str(val)
+        pass
+
+
+
+
+
+
+# print request time (in milliseconds)
+print response['msec']
+
+#print response
+# print mpn's
+for result in response['results']:
+    for item in result['items']:
+        #print item['offers'][0]['prices']
+        try:
+            print item['mpn']
+            #print item['price']
+            #print item['specs']
+            print item['specs']['supply_voltage_dc']['display_value']
+            print item['specs']['case_package']['display_value']
+        except:
+            print "this item doesn't have that attribute"
