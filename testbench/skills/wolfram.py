990a800854bc80762324402297454be4be503b4c
jazzrider <root@nat-tvwna-inside-visitornet101-c-23842.princeton.org>
2016-04-02 10:49:45 -0400

    third time's the charm?

--- testbench/skills/wolfram.py
+++ testbench/skills/wolfram.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 import urllib
-import urllib.request
 import util
 from skills.skill import SkillBase
 
@@ -17,7 +16,7 @@ class Wolfram(SkillBase):
         url += "appid=238HJV-7G3G7G8VYU&input="
         url += query
         url += "&format=image,plaintext"
-        data = urllib.request.urlopen(url).read()
+        data = urllib.urlopen(url).read()
         response = ET.fromstring(data)
 
         speech = ""
