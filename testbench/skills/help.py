bc64b4072737b6f2354cff28cdd588e650a8439a
jazzrider <root@nat-tvwna-inside-visitornet101-c-23842.princeton.org>
2016-04-02 07:44:19 -0400

    the answer

--- testbench/skills/help.py
+++ testbench/skills/help.py
@@ -13,3 +13,17 @@ class Help(SkillBase):
         should_end_session = False
         return util.build_response(session_attributes, util.build_speechlet_response(
             card_title, speech_output, reprompt_text, should_end_session))
+
+
+class GuideToGalaxy(SkillBase):
+
+    def execute(__self__, intent, session):
+        session_attributes = {}
+        card_title = "The answer"
+        speech_output = "42"
+        # If the user either does not reply to the welcome message or says something
+        # that is not understood, they will be prompted again with this text.
+        reprompt_text = None
+        should_end_session = False
+        return util.build_response(session_attributes, util.build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
