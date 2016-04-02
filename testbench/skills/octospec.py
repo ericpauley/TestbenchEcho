f7a5dea9efe3a31e7da0ab9f3c58258309c37386
venividivici440 <venividivici440@gmail.com>
2016-04-02 10:01:40 -0400

    utterances

--- testbench/skills/octospec.py
+++ testbench/skills/octospec.py
@@ -213,16 +213,65 @@ specMap = {
 "wire guage": 'wire_guage'
 }
 
+units = [
+  "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+  "nine"
+]
+
+def text2int(textnum, numwords={}):
+    if not numwords:
+      units = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+      ]
+
+      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+      scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+      numwords["and"] = (1, 0)
+      for idx, word in enumerate(units):    numwords[word] = (1, idx)
+      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+          raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
+
 
 class OctoSpec(SkillBase):
     def execute(__self__, intent, session):
         session_attributes = {}
 
+        mpnpre = intent['slots']['mpn']['value']
+
+        splitArray = mpnpre.split(' ')
+        for index in range(len(splitArray)):
+            for unit in units:
+                if splitArray[index].count(unit) >= 1:
+                    splitArray[index] = str(text2int(splitArray[index]))
+                    break
+
+
+        mpn = ''.join(splitArray)
+
         val = ""
         url = 'http://octopart.com/api/v3/parts/match?'
         url += '&apikey=0c491965'
         url += '&pretty_print=false'
-        url += '&queries=[{"mpn":"MSP430"}]'
+        url += '&queries=[{"mpn":"'
+        url += mpn
+        url += '"}]'
         url += '&include[]=specs'
 
 
