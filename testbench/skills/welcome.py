ade40afe722cbc5c109b4b4e3145e7a7dd36c8d1
venividivici440 <venividivici440@gmail.com>
2016-04-03 02:56:09 -0400

    k

--- testbench/skills/welcome.py
+++ testbench/skills/welcome.py
@@ -3,4 +3,5 @@ from skills.skill import SkillBase
 class Welcome(SkillBase):
 
     def execute(self, intent, session):
-        return self.respond("Welcome to Double E testbench")
+        #return self.respond("Welcome to Double E testbench")
+        return self.respond("EEYY BOSS!")
