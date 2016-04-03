cb96e5bbe152938d8fc981bedfdb76216b6a2f8e
venividivici440 <venividivici440@gmail.com>
2016-04-03 03:34:16 -0400

    k

--- testbench/skills/octopart.py
+++ testbench/skills/octopart.py
@@ -363,4 +363,4 @@ class OctoPrice(SkillBase):
         maxPrice = maxPrice[:-3];
         response = "The price ranges from " + maxPrice + "$"  " to " + minPrice + "$"
 
-        return self.respond(response, "Price Breaks for " + item['mpn'], str(prices dict), None)
+        return self.respond(response, "Price Breaks for " + item['mpn'], str(prices dict))
